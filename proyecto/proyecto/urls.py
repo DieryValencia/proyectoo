3992404b6b3c2585e33058bd3e8f49e4ef919e9b
DieryValencia <diery.valencia@campusucc.edu.co>
2026-03-26 12:14:18 -0500

    add home styles and configure routes

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('diery/', include('Diery.urls')),
     path('jesus/', include('jesus_cv.urls')),
