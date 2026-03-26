301b84be87e4f9c4654f16424df763fc5e44c553
DieryValencia <diery.valencia@campusucc.edu.co>
2026-03-26 12:00:18 -0500

    configure routes for diery

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('diery/', include('Diery.urls')),
     path('jesus/', include('jesus_cv.urls')),
 ]
