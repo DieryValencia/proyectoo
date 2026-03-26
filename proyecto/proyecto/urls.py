c3dd37b3fdd0c9e1a6797ee964c25d1c9b92670f
Oskar Ortiz <okyjulian994@gmail.com>
2026-03-26 11:58:40 -0500

    configure routes for oskar cv

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('jesus/', include('jesus_cv.urls')),
+    path('oskar/', include('oskar_cv.urls')),
 ]
